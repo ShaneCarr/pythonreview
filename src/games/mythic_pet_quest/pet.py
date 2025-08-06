7aec913c33acaa0a246816097d03e773c2aab2cc
Shane <shanepcarr@gmail.com>
2025-08-05 21:27:21 -0700

    c

--- src/games/mythic_pet_quest/pet.py
+++ src/games/mythic_pet_quest/pet.py
@@ -10,28 +10,31 @@ class MythicPet:
     hunger: int = 5
     energy: int = 5
     strength: int = 5
+
     def __post_init__(self):
         print(f"MythicPet: {self.name} enters world")
+
     def status(self):
-        print("todo status")
+        print(f"Name: {self.name} hunger {self.hunger} energy {self.energy} strength {self.strength}\n")
     def feed(self):
-        print("todo feed")
+        print("todo feed\n")
     def rest(self):
-        print("todo rest")
+        print("todo rest\n")
     def train(self):
-        print("todo train")
+        print("todo train\n")
+
 def main():
     pet = MythicPet("dragon")
     print(pet.name)
 
     while True:
-        pet.status()
-
-        print("\nChoose an Action")
+        print("==========\n\nChoose an Action============")
         print("1 Feed")
         print("2 Rest")
         print("3 Train")
-        print("4 Quit game")
+        print("4 status")
+        print("5 Quit game")
+        print("==========\n\n")
 
         choice = input("Enter your choice: ")
         if choice == "1":
@@ -41,9 +44,12 @@ def main():
         elif choice == "3":
             pet.train()
         elif choice == "4":
+            pet.status()
+        elif choice == "5":
             break
         else:
             print("Invalid choice")
+
 if __name__ == "__main__":
     main()
 
